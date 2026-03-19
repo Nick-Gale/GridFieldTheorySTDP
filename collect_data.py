3c1788328cab0221588efe7b763b78008466df7b
Nicholas Gale <nick.gale1993@gmail.com>
2026-03-19 16:28:04 +1100

    Create a simple pong data collection loop

---
+++ collect_data.py
@@ -0,0 +1,86 @@
+import pygame
+import numpy as np
+import src.pong
+
+pygame.init()
+
+# Font that is used to render the text
+font20 = pygame.font.Font('freesansbold.ttf', 20)
+
+WHITE = (255, 255, 255)
+
+# Basic parameters of the screen
+width, height = 1000, 1000
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pong")
+
+
+clock = pygame.time.Clock()    
+fps = 60
+nFrames = 24600 # arbitrary number
+
+buffer_topology = 20 ## amount of blank canvas for buttons to wrap around
+data_matrix = np.zeros([width + buffer_topology,height + buffer_topology,nFrames])
+
+def main():
+    running = True
+
+    # Defining the objects
+    computer_0 = Computer(20, 0, 10, 100, 10, WHITE)
+    computer_1 = Computer(width-30, 0, 10, 100, 10, WHITE)
+    ball = Ball(width//2, height//2, 7, 7, WHITE)
+
+    list_of_computers = [computer_0, computer_1]
+    i = 0
+    while running:
+        screen.fill([0,0,0])
+
+        # Collision detection
+        for ai in list_of_computers:
+            if pygame.Rect.colliderect(ball.getRect(), ai.getRect()):
+                ball.hit()
+
+        # get the position before the frame 
+        computer_pre = computer_0.posy
+        # Updating the objects
+        computer_0.update(ball.posy)
+        computer_1.update(ball.posy)
+        point = ball.update()
+
+        if point:   
+            ball.reset()
+        
+        # get the position after the frame
+        computer_post = computer_0.posy
+        
+        # Displaying the objects on the screen
+        ball.display()
+        pygame.display.update()
+        clock.tick(fps)
+        
+        # collect screen data
+        
+        window_pixel_matrix = pygame.surfarray.array2d(screen)
+        
+        # augment paddle data to topological neural field
+        button = np.sign(computer_post - computer_pre)
+        data_matrix[0:width, 0:height, i] = window_pixel_matrix
+        if button == 1:
+            data_matrix[-buffer_topology,:,i] = 1
+            data_matrix[:,-buffer_topology,i] = -1
+        elif button == 0:
+            data_matrix[-buffer_topology,:,i] = -1
+            data_matrix[:,-buffer_topology,i] = 1
+        
+        i += 1
+        if i > np.shape(data_matrix)[-1]:
+            raise ValueError("Training data saturated")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        np.save("data/data", data_matrix)
+    pygame.quit()
+
---
+++ src/pong.py
@@ -0,0 +1,130 @@
+import pygame
+
+class Striker:
+        # Take the initial position, dimensions, speed and color of the object
+    def __init__(self, posx, posy, width, height, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.color = color
+        # Rect that is used to control the position and collision of the object
+        self.geekRect = pygame.Rect(posx, posy, width, height)
+        # Object that is blit on the screen
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
+
+    # Used to display the object on the screen
+    def display(self):
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
+
+    def update(self, yFac):
+        self.posy = self.posy + self.speed*yFac
+
+        # Restricting the striker to be below the top surface of the screen
+        if self.posy <= 0:
+            self.posy = 0
+        # Restricting the striker to be above the bottom surface of the screen
+        elif self.posy + self.height >= HEIGHT:
+            self.posy = HEIGHT-self.height
+
+        # Updating the rect with the new values
+        self.geekRect = (self.posx, self.posy, self.width, self.height)
+
+    def displayScore(self, text, score, x, y, color):
+        text = font20.render(text+str(score), True, color)
+        textRect = text.get_rect()
+        textRect.center = (x, y)
+
+        screen.blit(text, textRect)
+
+    def getRect(self):
+        return self.geekRect
+
+class Ball:
+    def __init__(self, posx, posy, radius, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.radius = radius
+        self.speed = speed
+        self.color = color
+        self.xFac = 1
+        self.yFac = -1
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius)
+        self.firstTime = 1
+
+    def display(self):
+        self.ball = pygame.draw.circle(
+            screen, self.color, (self.posx, self.posy), self.radius)
+
+    def update(self):
+        self.posx += self.speed*self.xFac
+        self.posy += self.speed*self.yFac
+
+        if self.posy <= 0 or self.posy >= HEIGHT:
+            self.yFac *= -1
+
+        if self.posx <= 0 and self.firstTime:
+            self.firstTime = 0
+            return 1
+        elif self.posx >= WIDTH and self.firstTime:
+            self.firstTime = 0
+            return -1
+        else:
+            return 0
+
+    def reset(self):
+        self.posx = WIDTH//2
+        self.posy = HEIGHT//2
+        self.xFac *= -1
+        self.firstTime = 1
+
+    def hit(self):
+        self.xFac *= -1
+
+    def getRect(self):
+        return self.ball
+
+class Computer:
+    def __init__(self, posx, posy, width, height, speed, color):
+        self.posx = posx
+        self.posy = posy
+        self.width = width
+        self.height = height
+        self.speed = speed
+        self.color = color
+        # Rect that is used to control the position and collision of the object
+        self.geekRect = pygame.Rect(posx, posy, width, height)
+        # Object that is blit on the screen
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
+
+    # Used to display the object on the screen
+    def display(self):
+        self.geek = pygame.draw.rect(screen, self.color, self.geekRect)
+
+    def update(self, yFac):
+        # update to ball position always
+        self.posy = yFac - 50
+
+        # Restricting the striker to be below the top surface of the screen
+        if self.posy <= 0:
+            self.posy = 0
+        # Restricting the striker to be above the bottom surface of the screen
+        elif self.posy + self.height >= HEIGHT:
+            self.posy = HEIGHT-self.height
+
+        # Updating the rect with the new values
+        self.geekRect = (self.posx, self.posy, self.width, self.height)
+
+    def displayScore(self, text, score, x, y, color):
+        text = font20.render(text+str(score), True, color)
+        textRect = text.get_rect()
+        textRect.center = (x, y)
+
+        screen.blit(text, textRect)
+
+    def getRect(self):
+        return self.geekRect
+
+
